cce9a0cd853ba7d6b0f72df196cfe6ab97d7dab2
jghil <jayjeonghil94@gmail.com>
2022-09-23 20:32:00 +0000

    worked on post request to query both tables. #1

--- database/schema.sql
+++ database/schema.sql
@@ -1,61 +1,48 @@
 set client_min_messages to warning;
-
 -- DANGER: this is NOT how to do it in the real world.
 -- `drop schema` INSTANTLY ERASES EVERYTHING.
 drop schema "public" cascade;
-
 create schema "public";
 
 CREATE TABLE "public"."users" (
-	"userId" serial NOT NULL,
-	"username" TEXT NOT NULL UNIQUE,
-	"hashedPassword" TEXT NOT NULL,
-	"createdAt" TIMESTAMP NOT NULL,
-	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+  "userId" serial NOT NULL,
+  "username" TEXT NOT NULL UNIQUE,
+  "hashedPassword" TEXT NOT NULL,
+  "createdAt" TIMESTAMPTZ NOT NULL default now(),
+  CONSTRAINT "users_pk" PRIMARY KEY ("userId")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
 CREATE TABLE "public"."recipes" (
-	"recipeId" serial NOT NULL,
-	"userId" integer NOT NULL,
-	"createdAt" TIMESTAMP NOT NULL,
-	"title" TEXT NOT NULL,
-	"description" TEXT NOT NULL,
-	"directions" TEXT NOT NULL,
-	"image" TEXT NOT NULL,
-	"prepTime" TEXT NOT NULL,
-	"cookTime" TEXT NOT NULL,
-	"cuisine" integer NOT NULL,
-	"beverage" BOOLEAN NOT NULL,
-	"breakfastBrunch" BOOLEAN NOT NULL,
-	"dinner" BOOLEAN NOT NULL,
-	"snack" BOOLEAN NOT NULL,
-	"lunch" BOOLEAN NOT NULL,
-	"skillLevel" integer NOT NULL,
-	"tags" TEXT NOT NULL,
-	CONSTRAINT "recipes_pk" PRIMARY KEY ("recipeId")
+    "recipeId" serial NOT NULL,
+    "userId" integer NOT NULL,
+    "createdAt" TIMESTAMPTZ NOT NULL default now(),
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "directions" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "prepTime" TEXT NOT NULL,
+    "cookTime" TEXT NOT NULL,
+    "cuisine" integer NOT NULL,
+    "beverage" BOOLEAN NOT NULL,
+    "breakfast" BOOLEAN NOT NULL,
+    "dinner" BOOLEAN NOT NULL,
+    "snack" BOOLEAN NOT NULL,
+    "lunch" BOOLEAN NOT NULL,
+    "skillLevel" integer NOT NULL,
+    "tags" TEXT NOT NULL,
+    CONSTRAINT "recipes_pk" PRIMARY KEY ("recipeId")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
 CREATE TABLE "public"."ingredients" (
-	"ingredientId" serial NOT NULL,
-	"recipeId" integer NOT NULL,
-	"ingredient" TEXT NOT NULL,
-	"amount" TEXT NOT NULL,
-	CONSTRAINT "ingredients_pk" PRIMARY KEY ("ingredientId")
+    "ingredientId" serial NOT NULL,
+    "recipeId" integer NOT NULL,
+    "ingredient" TEXT NOT NULL,
+    "amount" TEXT NOT NULL,
+    CONSTRAINT "ingredients_pk" PRIMARY KEY ("ingredientId")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-
 ALTER TABLE "recipes" ADD CONSTRAINT "recipes_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
-
 ALTER TABLE "ingredients" ADD CONSTRAINT "ingredients_fk0" FOREIGN KEY ("recipeId") REFERENCES "recipes"("recipeId");
--- server/index.js
+++ server/index.js
@@ -2,15 +2,58 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+const ClientError = require('./client-error');
+const pg = require('pg');
 
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+const jsonMiddleware = express.json();
 const app = express();
-
+app.use(jsonMiddleware);
 app.use(staticMiddleware);
 
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.post('/api/recipes', (req, res) => {
+  const { userId, title, description, directions, image, prepTime, cookTime, cuisine, beverage, breakfast, dinner, snack, lunch, skillLevel, tags, ingredient, amount } = req.body;
+  if (!title || !description || !directions || !prepTime || !cookTime || !cuisine || !beverage || !breakfast || !dinner || !snack || !lunch || !skillLevel || !tags || !ingredient || !amount) {
+    throw new ClientError(400, 'title, description, directions, prepTime, cookTime, cuisine, beverage, breakfast, dinner, snack, lunch, skillLevel, tags, ingredient, amount are required fields');
+  }
+  const sql = `
+  insert into "recipes" ("userId", "title", "description", "directions", "image", "prepTime", "cookTime", "cuisine", "beverage", "breakfast", "dinner", "snack", "lunch", "skillLevel", "tags")
+  values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+  returning *
+  `;
+  const sql2 = `
+  insert into "ingredients" ("ingredient", "amount", "recipeId")
+  select "recipeId" from "recipes"
+  values ($1, $2, $3)
+  returning *
+  `;
+  const params = [userId, title, description, directions, image, prepTime, cookTime, cuisine, beverage, breakfast, dinner, snack, lunch, skillLevel, tags];
+  const params2 = [ingredient, amount];
+  db.query(sql, params)
+    .then(result => {
+      let [recipe] = result.rows;
+      db.query(sql2, params2)
+        .then(result => {
+          recipe = {
+            ingredients: [{
+              ingredient, amount
+            }]
+          };
+          res.status(201).json(recipe);
+        });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'an unexpected error occurred'
+      });
+    });
 });
-
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
